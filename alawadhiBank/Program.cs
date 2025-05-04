c630dc74028fef2ba77757b8c634133d299bfba1
Amr204 <140981183+Amr204@users.noreply.github.com>
2025-05-04 23:07:54 +0300

    step 1

--- alawadhiBank/Program.cs
+++ alawadhiBank/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace alawadhiBank
+{
+
+
+    class Program
+    {
+        static void PrintWithColor(string text, string colorName)
+        {
+            if (Enum.TryParse(colorName, true, out ConsoleColor color))
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(text);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine($"Error: Color '{colorName}' not found. Using default color.");
+                Console.WriteLine(text);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            PrintWithColor("AL-AWADHI BANK", "blue");
+            Console.WriteLine(@"1- admin
+2- user
+3- press any key to quit");
+            int choise = Byte.Parse(Console.ReadLine());
+            switch (choise)
+            {
+                case 1:
+                    Console.WriteLine("later");
+                    break;
+                case 2:
+                    user user = new user("Amr Khaled", "pass");
+                    break;
+                case 3:
+                    Console.WriteLine("later");
+                    break;
+                default:
+                    Console.WriteLine("Invalid number");
+                    break;
+
+               
+
+            }
+        }
+    }
+}
---
+++ alawadhiBank/user.cs
@@ -0,0 +1,44 @@
+﻿namespace alawadhiBank
+{
+    class user
+    {
+        private Guid _id;
+        private string _name;
+        private string _password;
+
+        public Guid Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Can't be null!");
+                _password = value;
+            }
+        }
+
+        public user() { }
+
+        public user(string name, string password)
+        {
+
+            _id = Guid.NewGuid();
+            _name = name;
+            _password = password;
+        }
+
+
+    }
+}
